cfddf192f9eb1b89421cd80897b2281f20c3f294
David Lozzi <lozzi.david@bcg.com>
2019-07-12 12:26:53 -0400

    added files

---
+++ support.js
@@ -0,0 +1,28 @@
+// just call window.upsideDown() and enjoy!
+
+window.upsideDown = () => {
+    const music = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/161676/music.mp3');
+    music.play();
+
+    const body = document.getElementsByTagName('body')[0];
+    body.style.background = "radial-gradient(transparent, black), #C11B1F";
+
+    window.setTimeout(() => {
+        body.style.transition = 'all 10s ease 0s, transform 12s';
+        body.style.transform = 'rotate(180deg) scale(.9)';
+        body.style.filter = 'invert(1)';
+
+        window.setTimeout(() => {
+            fadeMusic();
+        }, 10500);
+    }, 1000);
+
+    const fadeMusic = () => {
+        setTimeout(() => {
+            music.volume = music.volumne !== 0 ? music.volume - 0.1 : 0;
+            if(music.volume > 0) {
+                fadeMusic();
+            }
+        }, 400);
+    }
+};
