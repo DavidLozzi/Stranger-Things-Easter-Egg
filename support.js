66891f8af8ce7d46b4508a3ea1bc93374d08e04c
David Lozzi <lozzi.david@bcg.com>
2019-07-19 16:31:51 -0400

    refactored to add start, stop and getStatus methods

--- support.js
+++ support.js
@@ -1,28 +1,60 @@
-// just call window.upsideDown() and enjoy!
+// just call window.upsideDown.start() and enjoy!
+// call window.upsideDown.stop() to reset
 
-window.upsideDown = () => {
-    const music = new Audio('https://raw.githubusercontent.com/DavidLozzi/Stranger-Things-Easter-Egg/master/music.mp3');
-    music.play();
-
-    const body = document.getElementsByTagName('body')[0];
-    body.style.background = "radial-gradient(transparent, black), #C11B1F";
-
-    window.setTimeout(() => {
+// eslint-disable-next-line func-names
+window.upsideDown = (function () {
+    let music = {};
+    let body = {};
+    let originalBody = {};
+    let status = '';
+  
+    const start = () => {
+      music = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/161676/music.mp3');
+      music.play();
+      status = 'started';
+  
+      body = document.getElementsByTagName('body')[0];
+      originalBody = { ...body };
+  
+      body.style.background = 'radial-gradient(transparent, black), #C11B1F';
+  
+      const fadeMusic = () => {
+        setTimeout(() => {
+          music.volume = Math.round(music.volume) > 0 ? music.volume - 0.1 : 0;
+          if (music.volume > 0) {
+            fadeMusic();
+          } else {
+            status = 'done';
+          }
+        }, 400);
+      };
+  
+      window.setTimeout(() => {
+        status = 'running';
         body.style.transition = 'all 10s ease 0s, transform 12s';
         body.style.transform = 'rotate(180deg) scale(.9)';
         body.style.filter = 'invert(1)';
-
+  
         window.setTimeout(() => {
-            fadeMusic();
+          fadeMusic();
         }, 10500);
-    }, 1000);
-
-    const fadeMusic = () => {
-        setTimeout(() => {
-            music.volume = music.volumne !== 0 ? music.volume - 0.1 : 0;
-            if(music.volume > 0) {
-                fadeMusic();
-            }
-        }, 400);
-    }
-};
+      }, 1000);
+    };
+  
+    const stop = () => {
+      music.pause();
+      music = null;
+      body.style = originalBody.style;
+      status = '';
+    };
+  
+    const getStatus = () => {
+      return status;
+    };
+  
+    return {
+      start,
+      stop,
+      getStatus
+    };
+  }());
